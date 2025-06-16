5e9864e18683026300ec2a20bac8f9d0faf5c36f
Jonas Requena Ramirez <jonasrq@hotmail.com>
2025-06-16 13:05:51 +0200

    Traduccion Precios

---
+++ src/WaHub.Shared/Models/Resouces/ResourceKeys.Pricing.cs
@@ -0,0 +1,49 @@
+namespace WaHub.Shared.Models.Resouces;
+
+public static partial class ResourceKeys
+{
+    // Pricing page
+    public const string Pricing_Title = "Pricing_Title";
+    public const string Pricing_Description = "Pricing_Description";
+    public const string Pricing_Monthly = "Pricing_Monthly";
+    public const string Pricing_Annual = "Pricing_Annual";
+    public const string Pricing_AnnualDiscount = "Pricing_AnnualDiscount";
+    public const string Pricing_FreePlan = "Pricing_FreePlan";
+    public const string Pricing_FreePlan_Description = "Pricing_FreePlan_Description";
+    public const string Pricing_ProPlan = "Pricing_ProPlan";
+    public const string Pricing_ProPlan_Description = "Pricing_ProPlan_Description";
+    public const string Pricing_BusinessPlan = "Pricing_BusinessPlan";
+    public const string Pricing_BusinessPlan_Description = "Pricing_BusinessPlan_Description";
+    public const string Pricing_Feature_Messages = "Pricing_Feature_Messages";
+    public const string Pricing_Feature_Instances = "Pricing_Feature_Instances";
+    public const string Pricing_Feature_Api = "Pricing_Feature_Api";
+    public const string Pricing_Feature_Webhooks = "Pricing_Feature_Webhooks";
+    public const string Pricing_Feature_Dashboard = "Pricing_Feature_Dashboard";
+    public const string Pricing_Feature_Support = "Pricing_Feature_Support";
+    public const string Pricing_Feature_Analytics = "Pricing_Feature_Analytics";
+    public const string Pricing_Feature_Integrations = "Pricing_Feature_Integrations";
+    public const string Pricing_Feature_Promotions = "Pricing_Feature_Promotions";
+    public const string Pricing_Feature_AccountManager = "Pricing_Feature_AccountManager";
+    public const string Pricing_Feature_24_7Support = "Pricing_Feature_24_7Support";
+    public const string Pricing_Feature_CustomWebhooks = "Pricing_Feature_CustomWebhooks";
+    public const string Pricing_Feature_AdvancedDashboard = "Pricing_Feature_AdvancedDashboard";
+    public const string Pricing_Feature_EnterpriseApi = "Pricing_Feature_EnterpriseApi";
+    public const string Pricing_Feature_UnlimitedInstances = "Pricing_Feature_UnlimitedInstances";
+    public const string Pricing_Button_Free = "Pricing_Button_Free";
+    public const string Pricing_Button_Trial = "Pricing_Button_Trial";
+    public const string Pricing_Button_Contact = "Pricing_Button_Contact";
+    public const string Pricing_MostPopular = "Pricing_MostPopular";
+    public const string Pricing_OriginalPrice = "Pricing_OriginalPrice";
+    public const string Pricing_FAQ_Title = "Pricing_FAQ_Title";
+    public const string Pricing_FAQ_ChangePlan_Q = "Pricing_FAQ_ChangePlan_Q";
+    public const string Pricing_FAQ_ChangePlan_A = "Pricing_FAQ_ChangePlan_A";
+    public const string Pricing_FAQ_Limit_Q = "Pricing_FAQ_Limit_Q";
+    public const string Pricing_FAQ_Limit_A = "Pricing_FAQ_Limit_A";
+    public const string Pricing_FAQ_NGO_Q = "Pricing_FAQ_NGO_Q";
+    public const string Pricing_FAQ_NGO_A = "Pricing_FAQ_NGO_A";
+    public const string Pricing_FAQ_Setup_Q = "Pricing_FAQ_Setup_Q";
+    public const string Pricing_FAQ_Setup_A = "Pricing_FAQ_Setup_A";
+    public const string Pricing_CTA_Title = "Pricing_CTA_Title";
+    public const string Pricing_CTA_Description = "Pricing_CTA_Description";
+    public const string Pricing_CTA_Button = "Pricing_CTA_Button";
+}
