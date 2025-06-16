95b00874bb1b15d48ad2a121f97c5b9273a59403
Jonas Requena Ramirez <jonasrq@hotmail.com>
2025-06-16 15:51:46 +0200

    Añadir traducciones y localización en la página de contacto
    Se han actualizado los textos de la página de contacto de WaHub para utilizar un servicio de localización. Se han añadido nuevas claves de recursos y se han actualizado los archivos de traducción en inglés, francés, portugués y español. También se han implementado mensajes de validación y alertas para mejorar la retroalimentación del usuario.

---
+++ src/WaHub.Shared/Models/Resouces/ResourceKeys.Login.cs
@@ -0,0 +1,18 @@
+namespace WaHub.Shared.Models.Resouces;
+
+public static partial class ResourceKeys
+{
+    // Login page
+    public const string Login_Email = "Login_Email";
+    public const string Login_Password = "Login_Password";
+    public const string Login_SigningIn = "Login_SigningIn";
+    public const string Login_NoAccount = "Login_NoAccount";
+    public const string Login_RegisterHere = "Login_RegisterHere";
+
+    // Validations
+    public const string Login_Validation_EmailRequired = "Login_Validation_EmailRequired";
+    public const string Login_Validation_EmailInvalid = "Login_Validation_EmailInvalid";
+    public const string Login_Validation_PasswordRequired = "Login_Validation_PasswordRequired";
+    public const string Login_Validation_EnterEmailPassword = "Login_Validation_EnterEmailPassword";
+    public const string Login_ErrorDuringLogin = "Login_ErrorDuringLogin";
+} 
